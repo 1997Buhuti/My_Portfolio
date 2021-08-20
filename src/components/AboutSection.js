26b6a8668eda71da714deaeadf0e2218417d8b62
Manakal <dp.manakal82@gmail.com>
2021-08-20 21:47:35 +0530

    Initial commit

---
+++ src/components/AboutSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { About, Description, Image } from '../styles';
+import styled from 'styled-components';
+//Images
+import myImage from '../img/myImage.jpg';
+import { titleAnim, fade, photoAnim } from '../animation';
+import { motion } from 'framer-motion';
+import Wave from './Wave';
+
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <motion.h2 variants={titleAnim}>Make your</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
+              your <span>dream project </span> come true
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
+          </Hide>
+        </div>
+        <motion.p variants={fade}>
+          Hi I'm Manakal a fullstack web developer,
+          Tech enthusiast.
+          Contact Me to build your dream web project.
+        </motion.p>
+        <motion.button variants={fade}>Contact Me</motion.button>
+      </Description>
+      <Image>
+        <motion.img variants={photoAnim} src={myImage} alt="camera guy" />
+      </Image>
+      <Wave />
+    </About>
+  );
+};
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+export default AboutSection;
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import styled from 'styled-components';
+//Animation
+import { motion } from 'framer-motion';
+import { pageAnimation, titleAnim } from '../animation';
+import { Carousel,Button } from 'react-bootstrap';
+
+const ContactUs = () => {
+  return (
+    <ContactStyle
+      style={{ background: '#fff' }}
+      variants={pageAnimation}
+      exit="exit"
+      initial="hidden"
+      animate="show"
+    >
+      <Button> Hi </Button>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17b62ac711f%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17b62ac711f%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22278.328125%22%20y%3D%22221.365625%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17b62ac711f%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17b62ac711f%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22278.328125%22%20y%3D%22221.365625%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17b62ac711f%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17b62ac711f%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22278.328125%22%20y%3D%22221.365625%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+        <div className="line2"></div>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Socials</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send us a message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Drop an email.</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
+  );
+};
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+  @media (max-width: 1500px) {
+    padding: 2rem;
+    font-size: 1rem;
+  }
+`;
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 5rem;
+  height: 5rem;
+  background: #353535;
+  @media (max-width: 1500px) {
+    display: none;
+  }
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+  @media (max-width: 1500px) {
+    h2 {
+      margin: 1rem 0rem;
+      font-size: 2.5rem;
+    }
+  }
+`;
+
+export default ContactUs;
