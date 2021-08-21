4c3548ed040b0ba02f7bfefc730724d6ca4cda96
Manakal <dp.manakal82@gmail.com>
2021-08-21 15:46:10 +0530

    accordian created

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -26,19 +26,17 @@ const FaqSection = () => {
       </h2>
       <AnimateSharedLayout>
         <div className="accordion" id="accordionExample">
-          <div className="card">
+          <div>
             <div className="card-header" id="headingOne">
-              <h5 className="mb-0">
-                <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                <h2 data-toggle="collapse" data-target="#collapseOne"
                         aria-expanded="true" aria-controls="collapseOne">
-                  Collapsible Group Item #1
-                </button>
-              </h5>
+                  Question #1
+                </h2>
             </div>
 
-            <div id="collapseOne" className="collapse show" aria-labelledby="headingOne"
+            <div id="collapseOne" className="collapse" aria-labelledby="headingOne"
                  data-parent="#accordionExample">
-              <div className="card-body">
+              <div className="answer">
                 <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
                 moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
                 Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
@@ -48,42 +46,44 @@ const FaqSection = () => {
               </div>
             </div>
           </div>
-          <div className="card">
+
+          <div>
             <div className="card-header" id="headingTwo">
-              <h5 className="mb-0">
-                <button className="btn btn-link collapsed" type="button" data-toggle="collapse"
-                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Collapsible Group Item #2
-                </button>
-              </h5>
+              <h2 data-toggle="collapse" data-target="#collapseTwo"
+                  aria-expanded="true" aria-controls="collapseOne">
+                Question #2
+              </h2>
             </div>
-            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-              <div className="card-body">
+
+            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo"
+                 data-parent="#accordionExample">
+              <div className="answer">
                 <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                  moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                  Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                  shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                  proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
                   aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
               </div>
             </div>
           </div>
-          <div className="card">
+
+          <div>
             <div className="card-header" id="headingThree">
-              <h5 className="mb-0">
-                <button className="btn btn-link collapsed" type="button" data-toggle="collapse"
-                        data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  Collapsible Group Item #3
-                </button>
-              </h5>
+              <h2 data-toggle="collapse" data-target="#collapseThree"
+                  aria-expanded="true" aria-controls="collapseOne">
+                Question #3
+              </h2>
             </div>
-            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-              <div className="card-body">
+
+            <div id="collapseThree" className="collapse" aria-labelledby="headingThree"
+                 data-parent="#accordionExample">
+              <div className="answer">
                 <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                  moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                  Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                  shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                  proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
                   aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
               </div>
             </div>
