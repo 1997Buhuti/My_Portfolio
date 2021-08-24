1eeab10464fabe5c6844dd90490ed03cfbaed54a
Manakal <dp.manakal82@gmail.com>
2021-08-24 13:30:28 +0530

    local images created

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -31,7 +31,7 @@ const Nav = () => {
           />
         </li>
         <li>
-          <Link to="">1. Contact Me</Link>
+          <Link to="/contact">1. Contact Me</Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: '0%' }}
